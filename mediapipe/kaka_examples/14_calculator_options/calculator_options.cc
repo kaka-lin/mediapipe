2b627a5b8aeedf93817556d5a5c3feed4f6c3e4d
Kaka <vn503024@gmail.com>
2023-04-27 18:21:17 +0800

    feat: added the example of `Calculator Options`

---
+++ mediapipe/kaka_examples/14_calculator_options/calculator_options.cc
@@ -0,0 +1,76 @@
+// Here I create a calculator to join two strings,
+// also I introduce a source node (a calculator with no inputs)
+
+#include <iostream>
+#include <string>
+
+#include "mediapipe/framework/calculator_framework.h"
+#include "mediapipe/framework/port/parse_text_proto.h"
+#include "mediapipe/framework/port/logging.h"
+#include "mediapipe/framework/port/status.h"
+
+namespace mediapipe {
+
+absl::Status RunMPPGraph() {
+  // Create MP graph as protobuf text format
+  std::string k_proto = R"pb(
+    input_stream: "input"
+    output_stream: "output"
+    node {
+      calculator: "GoblinCalculator"
+      input_stream: "input"
+      output_stream: "output"
+      options: {
+        [mediapipe.GoblinCalculatorOptions.ext] {
+          opt_a: 7.0
+          opt_b: 3.0
+        }
+      }
+    }
+  )pb";
+
+  // parse it
+  CalculatorGraphConfig config;
+  if (!ParseTextProto<CalculatorGraphConfig>(k_proto, &config)) {
+    return absl::InternalError("Cannot parse the graph config !");
+  }
+
+  // Create MP Graph and intialize it with config
+  CalculatorGraph graph;
+  MP_RETURN_IF_ERROR(graph.Initialize(config));
+
+  // Output streams
+  ASSIGN_OR_RETURN(OutputStreamPoller poller,
+                   graph.AddOutputStreamPoller("output"));
+
+  // Run the graph
+  MP_RETURN_IF_ERROR(graph.StartRun({}));
+
+  // Send input packets to the graph, stream "input", then close it.
+  for (int i = 0; i < 13; ++i) {
+    // MP_RETURN_IF_ERROR(graph.AddPacketToInputStream("input",
+    //                    MakePacket<double>(i*0.1).At(Timestamp(i))));
+    mediapipe::Timestamp ts(i);
+    mediapipe::Packet packet = mediapipe::MakePacket<double>(i*0.1).At(ts);
+    MP_RETURN_IF_ERROR(graph.AddPacketToInputStream("input", packet));
+  }
+  MP_RETURN_IF_ERROR(graph.CloseInputStream("input"));
+
+  // Get the output packets string.
+  mediapipe::Packet packet;
+  while (poller.Next(&packet)) {
+    std::cout << packet.Timestamp() << ": RECEIVED PACKET " << packet.Get<double>() << std::endl;
+  }
+
+  // Wait for the graph to finish, and return graph status
+  // = `MP_RETURN_IF_ERROR(graph.WaitUntilDone())` + `return mediapipe::OkStatus()`
+  return graph.WaitUntilDone();
+}
+} // namespace mediapipe
+
+int main(int argc, char** argv) {
+  google::InitGoogleLogging(argv[0]);
+  std::cout << "Example : Calculator Options" << std::endl;
+  CHECK(mediapipe::RunMPPGraph().ok());
+  return 0;
+}
---
+++ mediapipe/kaka_examples/14_calculator_options/goblin_calculator.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+#include "mediapipe/framework/calculator_framework.h"
+#include "mediapipe/framework/port/status.h"
+
+#include "mediapipe/kaka_examples/14_calculator_options/goblin_calculator.pb.h"
+
+namespace mediapipe {
+  // A calculator with options
+  // Applies the operation f(x) = a*x + b to a stream of real numbers
+  class GoblinCalculator : public CalculatorBase {
+   public:
+    static Status GetContract(CalculatorContract* cc) {
+      // specify a calculator with 1 input, 1 output, both of type double
+      cc->Inputs().Index(0).Set<double>();
+      cc->Outputs().Index(0).Set<double>();
+      return absl::OkStatus();
+    }
+
+    // Open method is executed once when starting the graph
+    // "It can be used to parse options"
+    Status Open(CalculatorContext *cc) override {
+      // Copy options to class fields
+      auto options = cc->Options<GoblinCalculatorOptions>();
+      a = options.opt_a();
+      b = options.opt_b();
+      std::cout << "GoblinCalculator::Open() : a = " << a << ", b = " << b << std::endl;
+      return OkStatus();
+    }
+
+    Status Process(CalculatorContext *cc) override {
+      // Receive the input packet
+      Packet p_in = cc->Inputs().Index(0).Value();
+
+      // Extract the double number
+      // A Packet is immutable, so we cannot edit it!
+      double x = p_in.Get<double>();
+      // Process the number
+      double y = x * a + b;
+
+      // Create the output packet, with the input timestamp
+      Packet p_out = MakePacket<double>(y).At(cc->InputTimestamp());
+      // Send it to the output stream
+      cc->Outputs().Index(0).AddPacket(p_out);
+      return absl::OkStatus(); // Never forget to say "OK"!
+    }
+
+   private:
+    double a, b;
+  };
+
+  REGISTER_CALCULATOR(GoblinCalculator);
+}
